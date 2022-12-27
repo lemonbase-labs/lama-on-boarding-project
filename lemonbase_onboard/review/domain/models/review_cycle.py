03ff1ac396339490fda54ade8edcbce59bd084c9
JongHyeok Choi <opendev.choi@gmail.com>
2022-12-27 17:00:20 +0900

    feat: reviewCycle 의 review_cycle_creator 필드명 creator 로 변경

--- lemonbase_onboard/review/domain/models/review_cycle.py
+++ lemonbase_onboard/review/domain/models/review_cycle.py
@@ -7,6 +7,7 @@ from review.domain.models.question import Question
 class ReviewCycle(models.Model):
     id = models.AutoField(primary_key=True)
     review_cycle_creator = models.ForeignKey(
+    creator = models.ForeignKey(
         Person, related_name="review_cycles", on_delete=models.DO_NOTHING
     )
     name = models.CharField(max_length=128)
