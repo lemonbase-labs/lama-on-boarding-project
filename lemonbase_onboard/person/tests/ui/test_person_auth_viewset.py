bfd2436ddeff6f85ec52b48082413afdc4f0f829
JongHyeok Choi <opendev.choi@gmail.com>
2023-01-03 11:46:15 +0900

    test: user 테스트 코드 추가

---
+++ lemonbase_onboard/person/tests/ui/test_person_auth_viewset.py
@@ -0,0 +1,82 @@
+from rest_framework.test import APIClient, APITestCase
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+from person.domain.models.person import Person
+
+
+class PersonAuthTests(APITestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        Person(
+            email="test1@email.com", password=make_password("password"), name="name"
+        ).save()
+
+    def test_회원_가입시_id가_이메일_형식이_아닐_경우_400(self):
+        resp = self.client.post(
+            "/person/register/",
+            {"email": "email", "password": "password", "name": "name"},
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_회원_가입시_id가_이메일_또는_비밀번호_또는_이름이_없는_경우_400(self):
+        # Case 1. Missing email
+        resp = self.client.post(
+            "/person/register/", {"password": "password", "name": "name"}
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Case 2. Missing password
+        resp = self.client.post(
+            "/person/register/", {"email": "email@email.com", "name": "name"}
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Case 3. Missing name
+        resp = self.client.post(
+            "/person/register/",
+            {"email": "email@email.com", "password": "password"},
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_회원_가입시_정상적으로_회원_가입이_됬을_경우_201(self):
+        resp = self.client.post(
+            "/person/register/",
+            {"email": "email@email.com", "password": "password", "name": "name"},
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+    def test_로그인시_없는_계정으로_요청한_경우_401(self):
+        resp = self.client.post(
+            "/person/login/", {"email": "email@email.com", "password": "password"}
+        )
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_로그인시_id는_맞지만_비밀번호는_틀린_정보로_요청한_경우_401(self):
+        resp = self.client.post(
+            "/person/register/",
+            {"email": "email@email.com", "password": "password", "name": "name"},
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        resp = self.client.post(
+            "/person/login/",
+            {"email": "email@email.com", "password": "password1"},
+        )
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_로그인시_맞는_정보로_요청한_경우_202(self):
+        resp = self.client.post(
+            "/person/login/", {"email": "test1@email.com", "password": "password"}
+        )
+        self.assertEqual(resp.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_로그아웃시_로그인_안한_상태일_경우_403(self):
+        resp = self.client.post("/person/logout/")
+        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_로그아웃시_로그인한_상태일_경우_204(self):
+        self.client.login(email="test1@email.com", password="password")
+        resp = self.client.post("/person/logout/")
+
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
